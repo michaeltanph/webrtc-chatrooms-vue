fb11a33591ce9e9ea3a06a5bda7808c6f96dd5b0
kingcarabao <mtan.intl@gmail.com>
2021-04-12 01:24:57 +0800

    delete commented code

--- peer-server/server.js
+++ peer-server/server.js
@@ -1,17 +1,7 @@
 const { PeerServer } = require('peer');
- 
-//If hosted either on local or as HTTPs
 
 const port = 3001;
 const peerServer = PeerServer({ port: port, path: '/' });
-
-// If hosted as HTTPS
-// const peerServer = PeerServer({ port: 3001, path: '/',
-// 	ssl: {
-//     		key: fs.readFileSync('/etc/testi/privkey.pem'),
-//     		cert: fs.readFileSync('/etc/testi/fullchain.pem')
-//   	}
-// });
 
 peerServer.on('connection', function(conn) {
     console.log("connected", conn.id)
